d71554040d45efebc5337b73e65327ef16e8e05a
Aariv <arivbits@gmail.com>
2025-07-26 22:42:51 +0530

    Count Frequencies

--- src/main/java/Step_01_Learn_the_Basics/Lec6_Basic_Hashing/_01CountFrequencies.java
+++ src/main/java/Step_01_Learn_the_Basics/Lec6_Basic_Hashing/_01CountFrequencies.java
@@ -1,5 +1,29 @@
 package Step_01_Learn_the_Basics.Lec6_Basic_Hashing;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Given an array nums of size n which may contain duplicate elements, 
+// return a list of pairs where each pair contains a unique element from the array and its frequency in the array.
+
 public class _01CountFrequencies {
 
+//	Input: nums = [1, 2, 2, 1, 3]
+//
+//	Output: [[1, 2], [2, 2], [3, 1]]
+//
+//			Explanation:
+//
+//			- 1 appears 2 times
+//
+//			- 2 appears 2 times
+//
+//			- 3 appears 1 time
+//
+//			Order of output can vary.
+	public List<List<Integer>> countFrequencies(int[] arr) {
+		Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+		return null;
+	}
 }
