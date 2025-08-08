15d9e773738308bc0fe343bf38a3bcf02ee11cb2
Aariv <arivbits@gmail.com>
2025-08-08 14:33:24 +0530

    MaxConsecutiveOnes

--- src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_12MaxConsecutiveOnes.java
+++ src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_12MaxConsecutiveOnes.java
@@ -2,4 +2,19 @@ package Step_03_Sovle_Problems_on_Arrays.Lec1.Easy;
 
 public class _12MaxConsecutiveOnes {
 
+	public static int findMaxConsecutiveOnes(int[] arr) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : arr) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0; // reset if zero
+            }
+        }
+
+        return maxCount;
+    }
 }
