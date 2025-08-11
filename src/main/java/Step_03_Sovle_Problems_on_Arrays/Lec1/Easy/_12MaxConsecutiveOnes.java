3514c80c6238beaa54cb2105d012d0e0ffe30296
Aariv <arivbits@gmail.com>
2025-08-11 23:38:42 +0530

    Longest

--- src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_12MaxConsecutiveOnes.java
+++ src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_12MaxConsecutiveOnes.java
@@ -1,5 +1,6 @@
 package Step_03_Sovle_Problems_on_Arrays.Lec1.Easy;
 
+// Max CX
 public class _12MaxConsecutiveOnes {
 
 	public static int findMaxConsecutiveOnes(int[] arr) {
@@ -16,5 +17,28 @@ public class _12MaxConsecutiveOnes {
         }
 
         return maxCount;
+    }
+	
+	public static int longestOnes(int[] arr, int k) {
+        int left = 0, right = 0;
+        int zeroCount = 0, maxLen = 0;
+
+        while (right < arr.length) {
+            if (arr[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > k) {
+                if (arr[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
     }
 }
