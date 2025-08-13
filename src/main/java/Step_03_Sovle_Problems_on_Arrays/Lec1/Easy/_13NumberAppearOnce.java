e06062fb097fa2db4ba2f569e040ef1c41b60223
Aariv <arivbits@gmail.com>
2025-08-13 23:28:41 +0530

    Number Appear once

--- src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_13NumberAppearOnce.java
+++ src/main/java/Step_03_Sovle_Problems_on_Arrays/Lec1/Easy/_13NumberAppearOnce.java
@@ -2,4 +2,11 @@ package Step_03_Sovle_Problems_on_Arrays.Lec1.Easy;
 
 public class _13NumberAppearOnce {
 
+	public static int findSingleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR
+        }
+        return result;
+    }
 }
