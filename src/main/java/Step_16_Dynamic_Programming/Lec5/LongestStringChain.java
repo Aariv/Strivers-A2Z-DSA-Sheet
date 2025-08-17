d136bbc82c52aeb24add2bdc0ae5c0594155476a
Aariv <arivbits@gmail.com>
2025-08-17 21:44:38 +0530

    Updated LongestStringChain

---
+++ src/main/java/Step_16_Dynamic_Programming/Lec5/LongestStringChain.java
@@ -0,0 +1,28 @@
+package Step_16_Dynamic_Programming.Lec5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestStringChain {
+
+	public static int longestStrChain(String[] words) {
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        
+        Map<String, Integer> dp = new HashMap<>();
+        int maxLen = 1;
+
+        for (String word : words) {
+            dp.put(word, 1); // min length
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                if (dp.containsKey(prev)) {
+                    dp.put(word, Math.max(dp.get(word), dp.get(prev) + 1));
+                }
+            }
+            maxLen = Math.max(maxLen, dp.get(word));
+        }
+        return maxLen;
+    }
+}
