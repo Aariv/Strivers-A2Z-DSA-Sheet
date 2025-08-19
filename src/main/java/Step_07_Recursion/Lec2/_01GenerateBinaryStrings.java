7b93057b2e14e1562b2ece0362668cf31a8749b6
Aariv <arivbits@gmail.com>
2025-08-19 23:12:11 +0530

    Generate binary Strings

--- src/main/java/Step_07_Recursion/Lec2/_01GenerateBinaryStrings.java
+++ src/main/java/Step_07_Recursion/Lec2/_01GenerateBinaryStrings.java
@@ -2,4 +2,16 @@ package Step_07_Recursion.Lec2;
 
 public class _01GenerateBinaryStrings {
 
+	public static void generate(int n, String current) {
+        if (current.length() == n) {
+            System.out.println(current);
+            return;
+        }
+        generate(n, current + "0");
+        generate(n, current + "1");
+    }
+
+    public static void main(String[] args) {
+        generate(3, "");
+    }
 }
