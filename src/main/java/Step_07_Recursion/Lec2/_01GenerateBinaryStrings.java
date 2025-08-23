8bd3e8cf0b54a880d88c099108c24d04c75a92d9
Aariv <arivbits@gmail.com>
2025-08-23 15:48:51 +0530

    GenerateBinaryStrings

--- src/main/java/Step_07_Recursion/Lec2/_01GenerateBinaryStrings.java
+++ src/main/java/Step_07_Recursion/Lec2/_01GenerateBinaryStrings.java
@@ -1,5 +1,8 @@
 package Step_07_Recursion.Lec2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _01GenerateBinaryStrings {
 
 	public static void generate(int n, String current) {
@@ -10,8 +13,25 @@ public class _01GenerateBinaryStrings {
         generate(n, current + "0");
         generate(n, current + "1");
     }
+	
+	public static void generateOptimized(int n, StringBuilder current, List<String> result) {
+        if (current.length() == n) {
+            result.add(current.toString());
+            return;
+        }
+        current.append("0");
+        generateOptimized(n, current, result);
+        current.deleteCharAt(current.length()-1);
+        current.append("1");
+        generateOptimized(n, current, result);
+        current.deleteCharAt(current.length()-1);
+    }
 
     public static void main(String[] args) {
-        generate(3, "");
+//        generate(3, "");
+        StringBuilder str = new StringBuilder();
+        List<String> result = new ArrayList<String>();
+        generateOptimized(3, str, result);
+        System.out.println(result);
     }
 }
