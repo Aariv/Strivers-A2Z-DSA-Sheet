d3ea090460e1543a5f45d45abd0663032d62a85a
Aariv <arivbits@gmail.com>
2025-09-06 23:10:58 +0530

    PostOrder Traversal

---
+++ src/main/java/Step_13_Binary_Trees/Lec1/_04PostOrderTraversal.java
@@ -0,0 +1,33 @@
+package Step_13_Binary_Trees.Lec1;
+
+import Step_13_Binary_Trees.Lec1._02Preorder_Traversal.TreeNode;
+
+/**
+ * Given root of binary tree, return the Inorder traversal of the binary tree.
+ */
+public class _04PostOrderTraversal {
+
+	public void inorder(TreeNode root) {
+		if(root == null) {
+			return;
+		}
+		// Visit Left
+		inorder(root.left);
+		// Visit Root
+		System.out.print(root.data + " ");
+		// Visit Right
+		inorder(root.right);
+	}
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		
+		_04PostOrderTraversal pt = new _04PostOrderTraversal();
+		pt.inorder(root);
+	}
+}
